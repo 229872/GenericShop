21b35ada1bdb11c512bd6d268252866e7c2cb42d
Bartłomiej Dygasiński <bartek.dygasinski00@gmail.com>
2023-10-07 12:19:37 +0200

    Generated frontend and backend projects

---
+++ YerbaCafeTeaShopBackend/src/main/java/pl/lodz/p/edu/YerbaCafeTeaShopBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YerbaCafeTeaShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YerbaCafeTeaShopBackendApplication.class, args);
+	}
+
+}
