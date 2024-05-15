e0f1accbf0eff204f18ad73fd7c3e6a525b8b325
Bartłomiej Dygasiński <bartek.dygasinski00@gmail.com>
2024-05-15 17:15:04 +0200

    Added redirection for / to home

--- GenericShopFrontend/src/components/singleuse/Routing.tsx
+++ GenericShopFrontend/src/components/singleuse/Routing.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import AuthenticationPage from "../../pages/AuthenticationPage";
 import { SessionDialogsActions } from "../../utils/types";
 import RegisterPage from "../../pages/RegisterPage";
@@ -7,7 +7,7 @@ import NotFoundPage from "../../pages/NotFoundPage";
 export default function Routing({showTokenExpiredDialogAfterTimeout, showExtendSessionDialogAfterTimeout, setLoading}: SessionDialogsActions) {
   return (
     <Routes>
-      
+      <Route path='/' element={<Navigate replace to='/home' />} />
       <Route path='/auth' element={
         <AuthenticationPage
           showTokenExpiredDialogAfterTimeout={showTokenExpiredDialogAfterTimeout}
@@ -15,9 +15,7 @@ export default function Routing({showTokenExpiredDialogAfterTimeout, showExtendS
           setLoading={setLoading}
         />
       } />
-
       <Route path='/register' element={<RegisterPage setLoading={setLoading}/>} />
-
       <Route path='*' element={<NotFoundPage />} />
     </Routes>
   )
